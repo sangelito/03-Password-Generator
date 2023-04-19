23449046cae8aab19568a7c5bab2034e5333d342
sangelito <129316135+sangelito@users.noreply.github.com>
2023-04-19 09:45:17 -0500

    numbers and special characters defined

--- Develop/script.js
+++ Develop/script.js
@@ -17,6 +17,11 @@ var toUpper = function (x) {
   return x.toUpperCase(); 
 }; 
 
+upperCase = lowerCase.map(toUpper)
+
+var numbers = (0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+var speacial = ("!, @, $, $, %, ^, &, *, (, ), =, +, <, >, ?, ~,")
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
